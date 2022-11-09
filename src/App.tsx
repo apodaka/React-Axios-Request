8b1d9545d2a902d2ffc873e83edb25c1c18cb58c
Erubiel Apodaca <jesuserubiel.apodaca@gmail.com>
2022-11-09 01:30:38 -0700

    merge development

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,7 @@ import Example from "./containers/HomePage";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Hola Erubiel Apodaca</h1>
       <Example />
     </div>
   );
